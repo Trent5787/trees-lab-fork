95db1f93ed2ceaacd319203f7990dcb7537013f4
Cochell <cochellt@pnueli.cs.grinnell.edu>
2025-03-29 16:16:50 -0500

    We did it

--- src/main/java/edu/grinnell/csc207/trees/BinarySearchTree.java
+++ src/main/java/edu/grinnell/csc207/trees/BinarySearchTree.java
@@ -1,5 +1,6 @@
 package edu.grinnell.csc207.trees;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,11 +9,11 @@ import java.util.List;
 public class BinarySearchTree<T extends Comparable<? super T>> {
 
     ///// From the reading
-
     /**
      * A node of the binary search tree.
      */
     private static class Node<T> {
+
         T value;
         Node<T> left;
         Node<T> right;
@@ -41,7 +42,8 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
     /**
      * Constructs a new empty binary search tree.
      */
-    public BinarySearchTree() { }
+    public BinarySearchTree() {
+    }
 
     private int sizeH(Node<T> node) {
         if (node == null) {
@@ -79,64 +81,196 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
     }
 
     ///// Part 1: Traversals
-
     /**
      * @return the elements of this tree collected via an in-order traversal
      */
     public List<T> toListInorder() {
-        throw new UnsupportedOperationException();
+        ArrayList<T> lst = new ArrayList<T>();
+        return toListInorderHelper(lst, root);
+    }
+
+    public List<T> toListInorderHelper(List<T> lst, Node<T> cur) {
+        Node<T> temp = cur;
+        ArrayList<T> tmplst = new ArrayList<T>();
+        while (temp != null) {
+            tmplst.add(temp.value);
+            temp = temp.left;
+        }
+
+        for (int i = tmplst.size() - 1; i >= 0; i--) {
+            lst.add(tmplst.get(i));
+        }
+        if (cur.right != null) {
+            lst = toListInorderHelper(lst, cur.right);
+        }
+
+        return lst;
     }
 
     /**
      * @return the elements of this tree collected via a pre-order traversal
      */
     public List<T> toListPreorder() {
-        throw new UnsupportedOperationException();
+        ArrayList<T> lst = new ArrayList<T>();
+        return toListPreorderHelper(lst, root);
+    }
+
+    public List<T> toListPreorderHelper(List<T> lst, Node<T> cur) {
+        if (cur != null) {
+            lst.add(cur.value);
+            if (cur.left != null) {
+                lst = toListPreorderHelper(lst, cur.left);
+            }
+            if (cur.right != null) {
+                lst = toListPreorderHelper(lst, cur.right);
+            }
+        }
+        return lst;
     }
 
     /**
      * @return the elements of this tree collected via a post-order traversal
      */
     public List<T> toListPostorder() {
-        throw new UnsupportedOperationException();
+        ArrayList<T> lst = new ArrayList<T>();
+        return toListPostorderHelper(lst, root);
+    }
+
+    public List<T> toListPostorderHelper(List<T> lst, Node<T> cur) {
+        if (cur != null) {
+            if (cur.left != null) {
+                lst = toListPostorderHelper(lst, cur.left);
+            }
+            if (cur.right != null) {
+                lst = toListPostorderHelper(lst, cur.right);
+            }
+            lst.add(cur.value);
+        }
+        return lst;
     }
 
     ///// Part 2: Contains
-
     /**
      * @param value the value to search for
      * @return true iff the tree contains <code>value</code>
      */
     public boolean contains(T value) {
-        throw new UnsupportedOperationException();
+        ArrayList lst = new ArrayList();
+        lst = containsHelper(root, value, lst);
+        for (int i = 0; i < lst.size(); i++) {
+            if (lst.get(i).equals(true)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList containsHelper(Node<T> cur, T value, ArrayList lst) {
+        if (cur != null) {
+            if (cur.value == value) {
+                lst.add(true);
+            } else {
+                lst.add(false);
+            }
+            if (cur.left != null) {
+                lst = containsHelper(cur.left, value, lst);
+            }
+            if (cur.right != null) {
+                lst = containsHelper(cur.right, value, lst);
+            }
+        }
+        return lst;
+
     }
 
     ///// Part 3: Pretty Printing
-
     /**
-     * @return a string representation of the tree obtained via an pre-order traversal in the
-     *         form: "[v0, v1, ..., vn]"
+     * @return a string representation of the tree obtained via an pre-order
+     * traversal in the form: "[v0, v1, ..., vn]"
      */
     public String toStringPreorder() {
-        throw new UnsupportedOperationException();
+        StringBuffer buf = new StringBuffer("[");
+        buf = toStringPreorderHelper(root, root, buf);
+        buf.append("]");
+        return buf.toString();
+    }
+
+    public <T extends Comparable<? super T>> StringBuffer toStringPreorderHelper(Node<T> cur, Node<T> root, StringBuffer buf) {
+        if (cur != null) {
+            if (cur.equals(root)) {
+                buf.append(cur.value);
+            } else {
+                buf.append(", " + cur.value);
+            }
+            if (cur.left != null) {
+                buf = toStringPreorderHelper(cur.left, root, buf);
+            }
+            if (cur.right != null) {
+                buf = toStringPreorderHelper(cur.right, root, buf);
+            }
+        }
+        return buf;
     }
 
     ///// Part 4: Deletion
-  
     /*
      * The three cases of deletion are:
-     * 1. (TODO: fill me in!)
-     * 2. (TODO: fill me in!)
-     * 3. (TOOD: fill me in!)
+     * 1. No nodes beneath
+     * 2. One child
+     * 3. Two children
      */
-
     /**
-     * Modifies the tree by deleting the first occurrence of <code>value</code> found
-     * in the tree.
+     * Modifies the tree by deleting the first occurrence of <code>value</code>
+     * found in the tree.
      *
      * @param value the value to delete
      */
     public void delete(T value) {
-        throw new UnsupportedOperationException();
+        if (contains(value)) {
+            //ArrayList<Node<T>> lst = new ArrayList();
+            //lst = deleteHelper1(root, value, lst);
+            //Node<T> temp = lst.get(0);
+            root = deleteHelper2(root, value);
+
+        }
+
+    }
+
+    public Node<T> deleteHelper2(Node<T> cur, T value) {
+
+        if (cur != null) {
+            if (cur.value == value) {
+                if (cur.right == null && cur.left == null) {
+                    cur = null;
+                } else if (cur.right == null) {
+                    cur = cur.left;
+                } else if (cur.left == null) {
+                    cur = cur.right;
+                } else {
+                    Node<T> temp = cur;
+                    temp = temp.left;
+                    while (temp.right != null) {
+                        temp = temp.right;
+                    }
+                    cur.value = temp.value;
+                    if (temp.right == null && temp.left == null) {
+                        temp = null;
+                    } else if (temp.right == null) {
+                        temp = temp.left;
+                    } else if (temp.left == null) {
+                        temp = temp.right;
+                    }
+                }
+                return cur;
+            }
+            if (cur.left != null) {
+                cur.left = deleteHelper2(cur.left, value);
+            }
+            if (cur.right != null) {
+                cur.right = deleteHelper2(cur.right, value);
+            }
+
+        }
+        return cur;
     }
 }
